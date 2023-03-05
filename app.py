70257217c0bb606d17e5de4e78ff74cfd28a55c0
ztgeng <zhtgeng@gmail.com>
2023-03-04 16:52:32 -0800

    openai project

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
